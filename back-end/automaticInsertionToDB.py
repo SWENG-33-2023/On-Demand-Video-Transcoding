2cb6e6ebeb92641201ca82c6b63d029cd7911dfb
alexander <sepelena@tcd.ie>
2023-04-03 13:18:15 +0000

    Fixing incorrect automaticInsertionToDB pointing folder

--- back-end/automaticInsertionToDB.py
+++ back-end/automaticInsertionToDB.py
@@ -57,7 +57,8 @@ def getResolution(name, fileFolder):
         __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
         parentDir = os.path.abspath(os.path.join(__location__, os.pardir))
         print(parentDir)
-        getFileInfoCommandArray = ['ffprobe', "-v", "error", "-select_streams", "v", "-show_entries", "stream=width,height", "-of", "csv=p=0:s=x", parentDir + "/front-end/output-videos/" + name]
+        #getFileInfoCommandArray = ['ffprobe', "-v", "error", "-select_streams", "v", "-show_entries", "stream=width,height", "-of", "csv=p=0:s=x", parentDir + "/front-end/output-videos/" + name]
+        getFileInfoCommandArray = ['ffprobe', "-v", "error", "-select_streams", "v", "-show_entries", "stream=width,height", "-of", "csv=p=0:s=x", parentDir + "/back-end/assets/" + name]
         ffprobeProcess = subprocess.Popen(getFileInfoCommandArray, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         ffprobeResult, ffprobeError = ffprobeProcess.communicate()
 
